4b4869ed98453fbbc43b25d7e59837ffaa9d4daa
maxim.plotnikov <maxim.plotnikov@adonis.no>
2020-01-29 11:55:07 +0200

    Added navbar

---
+++ Planner.UI/Scripts/Planner/toolbar.js
@@ -0,0 +1,16 @@
+﻿
+var itemsToolbar = [
+    { text: "Sprints", url: "", icon: "runner" },
+    { text: "Tickets", icon: "clock" },
+    { text: "Members", icon: "group"},
+    { text: "Chart", icon: "chart" }
+];
+function InitNavBar () {
+    $("#navigation").dxNavBar({
+        dataSource: itemsToolbar,
+        selectionMode: "single",
+        selectedIndex:1,
+        onItemSelectionChanged: function(e) {
+        }
+    });
+}
